7687e29ca8d9d9e40e5700b1b028ff2bbf38da96
Shawn Bower <shawn.bower@gmail.com>
2012-02-05 17:44:26 -0500

    custom tweaks

--- lib/public_activity/common.rb
+++ lib/public_activity/common.rb
@@ -29,10 +29,10 @@ module PublicActivity
         if !Pusher.app_id.nil? && !Pusher.key.nil? && !Pusher.secret.nil?
          picture_url = ""
          
-         if trackable_type.eql?("Program")
-           picture_url = news_feed.trackable.organization.logo.profile.url
-         elsif news_feed.trackable_type.eql?("Task")
-           picture_url = trackable.program.organization.logo.profile.url
+         if activity.trackable_type.eql?("Program")
+           picture_url = activity.trackable.organization.logo.profile.url
+         elsif activity.trackable_type.eql?("Task")
+           picture_url = activity.trackable.program.organization.logo.profile.url
          end
             
           Pusher['acitivty-channel'].trigger('acitivty-create', {:picture_url => picture_url, :key => key, :owner => owner, :parameters => params, 
